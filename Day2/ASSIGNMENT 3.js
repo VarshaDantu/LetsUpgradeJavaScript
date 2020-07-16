9fe1beb71eaa1a245dbe5637c425f65c386c8138
Varsha Dantu <66246896+VarshaDantu@users.noreply.github.com>
2020-07-16 20:36:49 +0530

    Update and rename ASSIGNMENT 3 to ASSIGNMENT 3.js

--- Day2/ASSIGNMENT 3.js
+++ Day2/ASSIGNMENT 3.js
@@ -1,4 +1,4 @@
-Ask the user if he is 21+ and log the value "Can drink", "Cannot Drink".
+//Ask the user if he is 21+ and log the value "Can drink", "Cannot Drink".
 var a = window.prompt("Enter the age : ");
 var b = parseInt(a);
 if(b > 21)
