33debc2ff81dcf4e4e77ace952a0639e0772fa94
Varsha Dantu <66246896+VarshaDantu@users.noreply.github.com>
2020-07-16 21:03:29 +0530

    Create ASSIGNMENT-2.js

---
+++ DAY3/ASSIGNMENT-2.js
@@ -0,0 +1,6 @@
+//2.Write a program which will take OS name and version from the user separated by a space. Then
+// log the OS name and version on the console. Input: "Android 9"
+ //Output: The OS name is Android and version is 9
+ 
+ 
+ 
