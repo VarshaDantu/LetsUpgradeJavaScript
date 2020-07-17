5e9ecb2e1c690ea9951a08f533cce1e25560964c
Varsha Dantu <66246896+VarshaDantu@users.noreply.github.com>
2020-07-17 11:33:42 +0530

    Update ASSIGNMENT-2.js

--- DAY3/ASSIGNMENT-2.js
+++ DAY3/ASSIGNMENT-2.js
@@ -2,5 +2,8 @@
 // log the OS name and version on the console. Input: "Android 9"
  //Output: The OS name is Android and version is 9
  
- 
+ let str=prompt("Enter OS and version");
+
+let str1=str.split(' ');
+console.log("The OS name is "+str1[0]+" and version is "+str1[1]);
  
